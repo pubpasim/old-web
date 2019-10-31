aa809701420e1ac4639b8b383d2fb9edfd95f9ad
IyanSanjaya12 <iyanyanosanjaya@gmail.com>
2019-10-31 16:33:07 +0700

    data

--- resources/views/User/header.blade.php
+++ resources/views/User/header.blade.php
@@ -165,7 +165,6 @@
       </div>
     </div>
   </header>
-</body>
   <div class="modal" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
     <div class="modal-dialog" role="document">
       <div class="modal-content">
